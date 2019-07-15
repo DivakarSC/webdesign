55ebfc87e2dba588c93384898ed0ddb23dc56b2c
DivakarSC <rvsdiva6022@gmail.com>
2019-07-16 01:38:01 +0530

    helloworld program with http request and response

--- webdesign/urls.py
+++ webdesign/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webdesign.helloworld import bar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('HelloWorldApp/', bar),
 ]
